80a0b5911823d1900846a51795251d517419b5e7
Hunter Merrill <117316321+hunter-merrill@users.noreply.github.com>
2025-08-14 01:53:23 -0500

    Time: 3 ms (59.17%) | Memory: 13.3 MB (31.26%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,15 @@
+# Hash table approach: bypasses need to search list for complement
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashmap = {}
+        length = len(nums)
+        
+        for i in range(length):
+            curr = nums[i]
+            complement = target - curr
+            
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            
+            hashmap[curr] = i

0af1952f3780b241b65158a7dbb22885e5879988
Hunter Merrill <117316321+hunter-merrill@users.noreply.github.com>
2025-08-14 01:52:09 -0500

    Time: 2 ms (61.57%) | Memory: 12.5 MB (56.86%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution(object):
+    def isValid(self, s):
+        d = deque()
+        d.append('start')
+        
+        # Dictionaries to check if brackets match
+        openPar = {
+            '(': ')', 
+            '[': ']', 
+            '{': '}'
+        }
+        closedPar = {
+            ')': '(', 
+            ']': '[', 
+            '}': '{'
+        }
+        
+        # Iterate through characters in s
+        for c in s:
+            # Append (, [, { to deque
+            if c in openPar:
+                d.append(c)
+            
+            # Check ), ], } for matching open
+            elif c in closedPar:
+                if d.pop() != closedPar[c]:
+                    return False
+        
+        # Check that all open brackets are closed
+        return d.pop() == 'start'

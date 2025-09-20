3d86f7209155a06158f86a013a564b5e8bb10301
Hunter Merrill <117316321+hunter-merrill@users.noreply.github.com>
2025-09-20 12:44:47 -0500

    Time: 1 ms (2.53%) | Memory: 18 MB (8.38%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,29 @@
+import math
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# Naive: traverse the whole list, tallying the # of elements
+# --> re-traverse from head up to numElements/2, return that node
+# 
+# Little better:
+# Store list in an array as you go, return arr[n//2]
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        length = 0
+
+        node = head
+        while node.next:
+            length += 1
+            node = node.next
+
+        node = head
+        for i in range(math.ceil(length / 2)):
+            node = node.next
+        
+        return node

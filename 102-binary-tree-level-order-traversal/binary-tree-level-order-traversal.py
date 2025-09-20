2bf9c97308abfccf00fb4045a11e3c3b37ae81d8
Hunter Merrill <117316321+hunter-merrill@users.noreply.github.com>
2025-09-20 15:11:29 -0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (39.04%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -13,30 +13,23 @@ class Solution:
             return []
 
         dq = deque([root])
-        currLevel = 1 # Nodes in current level
-        nextLevel = 0 # Nodes in next level (incremented as we go)
 
         def bfs(node):
-            nonlocal dq, currLevel, nextLevel
+            nonlocal dq
 
             # Add children (if they exist)
             for node in [node.left, node.right]:
                 if node:
                     dq.append(node)
-                    nextLevel += 1
         
-        levels = [[]]
+        levels = []
         while dq:
             
-            # Check if next level reached
-            if currLevel == 0:
-                currLevel = nextLevel # Move on to next level
-                nextLevel = 0 # Reset nodes in next level
-                levels.append([]) # Prepare list in which to add new children
-
-            currNode = dq.popleft() # Get oldest node in queue
-            levels[-1].append(currNode.val) # Add it to the current level
-            bfs(currNode) # Find its children
-            currLevel -= 1 # Decrease remaining nodes in level
+            # Run through nods in current level (= node in queue when level starts)
+            levels.append([])
+            for i in range(len(dq)):
+                currNode = dq.popleft()
+                levels[-1].append(currNode.val)
+                bfs(currNode)
         
         return levels
